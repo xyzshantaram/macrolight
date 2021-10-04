7936b0893fc5fce1530729c871b21e0d8e17f9e9
Siddharth Singh <simsid66@gmail.com>
2021-10-04 11:04:13 +0530

    fix element text-getting for nodejs

--- macrolight.js
+++ macrolight.js
@@ -11,15 +11,10 @@ const highlight = function (src, config = {}) {
 
     const isKeyword = (token) => {
         const keywords = config.keywords || [];
-
         return keywords.includes(token);
     }
 
-    if (!(typeof src === 'string') && !(src instanceof Element)) {
-        throw new Error("Invalid argument passed to highlight()");
-    }
-
-    const text = src instanceof Element ? src.textContent : src;
+    const text = 'textContent' in src ? src.textContent : src;
 
     let pos = 0; // current position
     let next = text[0]; // next character
