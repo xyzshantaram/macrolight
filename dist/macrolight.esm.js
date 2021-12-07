68d83adadb116ff2e7f9605a331c517a93f0158e
Siddharth Singh <simsid66@gmail.com>
2021-12-07 13:19:20 +0530

    version bump

--- dist/macrolight.esm.js
+++ dist/macrolight.esm.js
@@ -1,3 +1,20 @@
+// hl-keywords.js
+var HL_KEYWORDS = {
+  typescript: ["goto", "in", "instanceof", "static", "finally", "arguments", "public", "do", "else", "const", "function", "class", "return", "let", "catch", "eval", "for", "if", "this", "try", "break", "debugger", "yield", "extends", "enum", "continue", "export", "null", "switch", "private", "new", "throw", "while", "case", "await", "delete", "super", "default", "void", "var", "protected", "package", "interface", "false", "typeof", "implements", "with", "import", "true"],
+  c: ["auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"],
+  html: ["doctype", "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bb", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datagrid", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "eventsource", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "isindex", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"],
+  python: ["False", "await", "else", "import", "pass", "None", "break", "except", "in", "raise", "True", "class", "finally", "is", "return", "and", "continue", "for", "lambda", "try", "as", "def", "from", "nonlocal", "while", "assert", "del", "global", "not", "with", "async", "elif", "if", "or", "yield", "map", "__init__", "sum", "list", "input"],
+  rust: ["union", "'static", "dyn", "as", "break", "const", "continue", "crate", "else", "enum", "extern", "false", "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod", "move", "mut", "pub", "ref", "return", "self", "Self", "static", "struct", "super", "trait", "true", "type", "unsafe", "use", "where", "while", "async", "await", "dyn", "abstract", "become", "box", "do", "final", "macro", "override", "priv", "typeof", "unsized", "virtual", "yield", "try"]
+};
+var HL_ALIASES = {
+  python: ["py"],
+  typescript: ["js", "ts", "javascript"],
+  rust: ["rs"]
+};
+for (const [key, val] of Object.entries(HL_ALIASES)) {
+  val.forEach((alias) => HL_KEYWORDS[alias] = HL_KEYWORDS[key]);
+}
+
 // macrolight.js
 var escape = (str) => {
   if (!str)
@@ -104,6 +121,7 @@ var highlightAll = (config = {}, selector = ".macrolight") => {
   });
 };
 export {
+  HL_KEYWORDS,
   highlight,
   highlightAll
 };
